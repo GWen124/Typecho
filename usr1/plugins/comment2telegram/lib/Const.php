35cc8c5d1cd3e62766be75011033353e30fd420c
GWen124 <9708281@qq.com>
2023-01-06 17:17:39 +0800

    Update

--- usr1/plugins/comment2telegram/lib/Const.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Plugin_Const {
-    const VERSION = '1.3.2';
-    
-    const EMAIL_SENT_API = 'https://api.aim.moe/Common/SendEmail';
-
-    const GITHUB_REPO_API = 'https://api.github.com/repos/MoeLoli/Comment2Telegram/releases/latest';
-}
